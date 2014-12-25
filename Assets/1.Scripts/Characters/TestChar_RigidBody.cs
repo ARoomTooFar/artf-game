047796f63cecc08fe55dcacd25267f900644df9d
Scipik <fltang@ucsc.edu>
2014-12-24 18:08:46 -0800

    Rigidbody as Controller
    Using the Rigidbody as the character controller is much smoother but there are 2 issues atm.
    1) Even when only holding left/right/up/down, there are slight movement changes in different direction.
    	It's small and not noticeble, especially if we use a arcade stick for movement, but I will look more inot it.
    2) Currently I need a better way to detect if unit is grounded, will try a collision check instead of raycasting.

---
+++ Assets/1.Scripts/Characters/TestChar_RigidBody.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TestChar_RigidBody : MonoBehaviour {
+
+	public float speed = 100.0f;
+	public float rotateSpeed = 3.0f;
+	
+	public float minGroundDistance; // How far this unit should be from the ground when standing up
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	void FixedUpdate() {
+		moveCommands ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	/* Seperate these functions into an interface/superclass later */
+	
+	void moveCommands() {
+		// Commands that only work on the ground go here
+		if (isGrounded()) {
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+			
+			Vector3 movement = new Vector3 (moveHorizontal, rigidbody.velocity.y, moveVertical);
+			rigidbody.velocity = movement.normalized * speed;
+		}
+	}
+	
+	// Checks if user is grounded by sending a raycast to the ground
+	// If called multiple times per update, consider making a bool variable and only call once per updaye
+	public bool isGrounded () {
+		return Physics.Raycast (transform.position, -Vector3.up, minGroundDistance);
+	}
+	
+	/****************************************************************/
+}
