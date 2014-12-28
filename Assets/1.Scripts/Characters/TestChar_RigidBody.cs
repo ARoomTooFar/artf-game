7ce0939189cbd5ed59e63505f10a3a5122e1f59a
Scipik <fltang@ucsc.edu>
2014-12-27 17:50:48 -0800

    Character with animation added
    Got a character from the asset store to test facing and animation.
    The rigidbody seems to work alright, excepty it bounces off walls and stuff.
    Might just code movement on my own instead of using the rigidbodies.
    
    To do before school:
    	Create simple classes so we can have 4 units.
    	Have several different perspectives (Current, Link style, Angled Camera, etc)
    	Mess with camera for the multiplayer

--- Assets/1.Scripts/Characters/TestChar_RigidBody.cs
+++ Assets/1.Scripts/Characters/TestChar_RigidBody.cs
@@ -4,35 +4,61 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class TestChar_RigidBody : MonoBehaviour {
 
-	public float speed = 100.0f;
+	public float speed = 5.0f;
+	public float gravity = 50.0f;
 	public float rotateSpeed = 3.0f;
+	
+	public Vector3 facing; // Direction unit is facing
 	
 	public float minGroundDistance; // How far this unit should be from the ground when standing up
 
+	private Animator animator;
+
 	// Use this for initialization
 	void Start () {
+		animator = this.GetComponent<Animator>();
+		facing = Vector3.forward;
 	}
 	
 	void FixedUpdate() {
-		
+		moveCommands ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		moveCommands ();
+		animationUpdate();
 	}
 	
 	/* Seperate these functions into an interface/superclass later */
 	
-	void moveCommands() {
+	private void moveCommands() {
 		// Commands that only work on the ground go here
 		if (isGrounded()) {
 			float moveHorizontal = Input.GetAxis ("Horizontal");
 			float moveVertical = Input.GetAxis ("Vertical");
 			
 			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			
 			rigidbody.velocity = movement.normalized * speed;
+		} else {
+			// fake gravity
+			// Animation make it so rigidbody gravity works oddly due to some gravity weight
+			// Seems like Unity Pro is needed to change that, so unless we get it, this will suffice 
+			rigidbody.velocity = new Vector3 (0.0f, -gravity, 0.0f);
 		}
+	}
+	
+	private void animationUpdate() {
+		Vector3 temp = rigidbody.velocity;
+		temp.y = 0.0f;
+		if (temp != Vector3.zero) {
+			animator.SetBool("Moving", true);
+			facing = rigidbody.velocity.normalized;
+			transform.rotation = Quaternion.LookRotation(facing);
+		} else {
+			animator.SetBool("Moving", false);
+		}
+		
 	}
 	
 	// Checks if user is grounded by sending a raycast to the ground
