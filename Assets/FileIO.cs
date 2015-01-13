76186d1338e0d75a8213d79cbc40caad171ef940
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-01-12 22:06:27 -0800

    committing all my junk from deleted branch

---
+++ Assets/FileIO.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO; 
+
+public class FileIO : MonoBehaviour {
+
+}
---
+++ Assets/StickToThing.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickToThing : MonoBehaviour {
+	public Transform thing;
+	public Camera cam;
+
+
+
+	public RectTransform rects;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		Vector3 p = new Vector3();
+		p = thing.transform.position;
+		float f = p.x;
+
+		
+		
+		//rects.transform.position = p;
+		transform.position = p;
+
+
+
+
+	}
+}
