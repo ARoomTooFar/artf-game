c78dfc0d94390676f6fd0cbc19309f58387510c3
Francis Tang <fltang@ucsc.edu>
2015-01-14 17:45:15 -0800

    Dagger Added

---
+++ Assets/1.Scripts/Equipment/Weapons/Daggers/DaggerMove.cs
@@ -0,0 +1,19 @@
+// Use for dagger movment while we don't have animations
+
+using UnityEngine;
+using System.Collections;
+
+public class DaggerMove : MonoBehaviour {
+	
+	public Transform refThrust;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (refThrust.position.x, refThrust.position.y, refThrust.position.z);
+	}
+}
