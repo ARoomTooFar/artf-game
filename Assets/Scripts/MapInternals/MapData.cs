0b3a6578c1025cf3b9285121abab1a82812ae55f
eazama <eazama001@gmail.com>
2015-01-14 22:05:56 -0800

    MapData now singleton

--- Assets/Scripts/MapInternals/MapData.cs
+++ Assets/Scripts/MapInternals/MapData.cs
@@ -4,7 +4,23 @@ using System.Collections.Generic;
 
 public class MapData {
 
+	private MapData(){}
+
+	private static MapData instance;
+	public static MapData getInstance(){
+		if(instance == null) {
+			instance = new MapData();
+		}
+		return instance;
+	}
+
 	private TerrainDictionary terrainBlocks = new TerrainDictionary ();
+	public TerrainDictionary TerrainBlocks{
+		get{
+			return terrainBlocks;
+		}
+	}
+	private SceneryDictionary sceneryBlocks = new SceneryDictionary();
 
 	public string getSaveString(){
 		return "";
