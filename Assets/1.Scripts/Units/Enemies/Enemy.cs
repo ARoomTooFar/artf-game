958be3a8cf6619fa95016aede3a0f1a91114edf6
calvinatlan <calvinatlan@gmail.com>
2015-01-16 17:02:42 -0800

    AI First Implementation
    To be added next: Turning to face player when close, attacking, aggro
    table

--- Assets/1.Scripts/Units/Enemies/Enemy.cs
+++ Assets/1.Scripts/Units/Enemies/Enemy.cs
@@ -1,26 +1,98 @@
-// Parent scripts for enemy units
+// Parent script for AI controlled enemies
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Enemy : MonoBehaviour, IDamageable<int> {
+
+[RequireComponent(typeof(Rigidbody))]
+public class Enemy : MonoBehaviour, IFallable, IDamageable<int> {
+	
+	public float speed = 5.0f;
+	public float gravity = 50.0f;
+	public bool isGrounded = false;
+	public bool actable = true; // Boolean to show if a unit can act or is stuck in an animation
+	
+	public Vector3 facing; // Direction unit is facing
+	public Vector3 curFacing; // A better facing var, will change and combine in future
+	
+	public float minGroundDistance; // How far this unit should be from the ground when standing up
+	
+	public Controls controls;
+	public Stats stats;
+	public CharItems charItems;
+	
+	public bool freeAnim;
+	
+	public bool invincible = false;
+	
+	// Animation variables
+	public Animator animator;
+	public AnimatorStateInfo animSteInfo;
+	protected int idleHash, runHash, atkHash;
 	
 	// Use this for initialization
 	protected virtual void Start () {
+		animator = GetComponent<Animator>();
+		facing = curFacing = Vector3.forward;
+		freeAnim = true;
+		setInitValues();
+		setAnimHash();
+	}
+	protected virtual void setInitValues() {
+	}
+	
+	// Gets hash code for animations (Faster than using string name when running)
+	protected virtual void setAnimHash() {
+		idleHash = Animator.StringToHash ("Base Layer.idle");
+		runHash = Animator.StringToHash ("Base Layer.run");
+		atkHash = Animator.StringToHash ("Base Layer.attack");
+	}
+	
+	protected virtual void FixedUpdate() {
 		
 	}
 	
 	// Update is called once per frame
 	protected virtual void Update () {
+		isGrounded = Physics.Raycast (transform.position, -Vector3.up, minGroundDistance);
 		
+		animSteInfo = animator.GetCurrentAnimatorStateInfo(0);
+		actable = (animSteInfo.nameHash == runHash || animSteInfo.nameHash == idleHash) && freeAnim;
+		
+		if (!isGrounded) {
+			falling();
+		}
 	}
 
+
+	//-------------------------------------------//
+
+
+	//----------------------------------//
+	// Falling Interface Implementation //
+	//----------------------------------//w
+	
+	public virtual void falling() {
+		// fake gravity
+		// Animation make it so rigidbody gravity works oddly due to some gravity weight
+		// Seems like Unity Pro is needed to change that, so unless we get it, this will suffice 
+		rigidbody.velocity = new Vector3 (0.0f, -gravity, 0.0f);
+	}
+	
+	//----------------------------------//
+
+	
+	
+	//---------------------------------//
+	// Damage Interface Implementation //
+	//---------------------------------//
+	
 	public virtual void damage(int dmgTaken) {
-		print ("Fuck: " + dmgTaken + " Damage taken");
+		if (!invincible) {
+			stats.health -= dmgTaken;
+		}
 	}
-
-	/*//Use for other shit maybe
-	public virtual void OnTriggerEnter(Collider other) {
-		damage (1);
-	}*/
+	
+	//----------------------------------//
 }
---
+++ Assets/1.Scripts/Units/Enemies/EnemySight.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySight : MonoBehaviour {
+
+	//Public variables to tweak in inspector
+	public float fov = 110f;
+	public bool playerInSight;
+	public Vector3 targetPosition;
+
+	//Private variables for use in player detection
+	private NavMeshAgent nav;
+	private Animator ani;
+	private SphereCollider col;
+	private GameObject[] players;
+	private GameObject target;
+
+	//Get players, navmesh and all colliders
+	void Awake (){
+		nav = GetComponent<NavMeshAgent> ();
+		col = GetComponent<SphereCollider> ();
+		ani = GetComponent<Animator> ();
+		players = GameObject.FindGameObjectsWithTag ("Player");
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		for (int i = 0; i <=3; i++) 
+		{
+			if (other.gameObject == players[i]) 
+			{
+				playerInSight = false;
+			
+				// Check angle of forward direction vector against the vector of enemy position relative to player position
+				Vector3 direction = other.transform.position - transform.position;
+				float angle = Vector3.Angle (direction, transform.forward);
+			
+				if (angle < fov * 0.5f) 
+				{
+					RaycastHit hit;
+					if (Physics.Raycast (transform.position + transform.up, direction.normalized, out hit, col.radius)) {
+
+						if (hit.collider.gameObject == players[i]) 
+						{
+
+							playerInSight = true;
+							//print ("Enemy can see me");
+							targetPosition = other.transform.position;
+							attackPlayer(players[i]);
+														
+						}
+					}
+				}
+			
+			}
+		}
+	}
+
+	void attackPlayer (GameObject player)
+	{
+		Vector3 distanceToTarget = transform.position - targetPosition;
+		if (distanceToTarget.sqrMagnitude > 6) 
+		{
+			nav.destination = targetPosition;
+			ani.SetBool ("Moving", true);
+		} else {
+			nav.Stop ();
+			ani.SetBool ("Moving", false);
+		}
+	}
+}
