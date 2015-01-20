a4e7c9be8bc58d07999494009da7b7325bc18c96
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-01-19 17:38:39 -0800

    circle health bar moving properly. works from a scrollbar currently, so needs to be tested with changing values in-game. object UI rotation is being erratic after the merge. need to fix.

---
+++ Assets/UIScripts/BindCircleForImage.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+[RequireComponent(typeof(Scrollbar))]
+public class BindCircleForImage : MonoBehaviour
+{
+    [SerializeField]
+    Image CircleImage;
+    [SerializeField]
+    Color start;
+    [SerializeField]
+    Color end;
+
+    [SerializeField]
+    Color current;
+
+    Scrollbar scrollbar { get { return GetComponent<Scrollbar>(); } }
+
+    void Start()
+    {
+        CircleImage.type = Image.Type.Filled;
+        CircleImage.fillMethod = Image.FillMethod.Radial360;
+        CircleImage.fillOrigin = 0;
+    }
+
+    void Update()
+    {
+        CircleImage.fillAmount = Mathf.Max( scrollbar.value,0.001f);
+        CircleImage.color = Color.Lerp(start, end, scrollbar.value);
+        current = Color.Lerp(start, end, scrollbar.value);
+    }
+
+     
+}
---
+++ Assets/UIScripts/BindCircleForShader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BindCircleForShader : MonoBehaviour 
+{
+    [SerializeField]
+    Color start;
+    [SerializeField]
+    Color end;
+
+    [SerializeField]
+    Material CircleMaterial;
+
+    Scrollbar scrollbar { get { return GetComponent<Scrollbar>(); } }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        CircleMaterial.SetFloat("_Angle", Mathf.Lerp(-3.14f, 3.14f, scrollbar.value));
+        CircleMaterial.SetColor("_Color", Color.Lerp(start, end, scrollbar.value));
+	}
+}
