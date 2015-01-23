e3685d97550b5ea661951e0e6f93f33fa95a392d
eazama <eazama001@gmail.com>
2015-01-23 01:27:28 -0800

    DIRECTION Extension functions added
    added methods to determine if a DIRECTION is an ordinal or cardinal
    direction

--- Assets/Scripts/MapInternals/Extensions.cs
+++ Assets/Scripts/MapInternals/Extensions.cs
@@ -17,6 +17,28 @@ public static class Extensions
 	}
 
 	/*
+	 * Extension method for DIRECTION enum.
+	 * 
+	 * Returns true if the direction is a cardinal direction (N S E W)
+	 * Returns false otherwise
+	 * 
+	 */
+	public static bool isCardinal(this DIRECTION dir){
+		return Mathf.Abs((int)dir) == 1 || Mathf.Abs((int)dir) == 2;
+	}
+
+	/*
+	 * Extension method for DIRECTION enum.
+	 * 
+	 * Returns true if the direction is an ordinal direction (NW NE SE SW)
+	 * Returns false otherwise
+	 * 
+	 */
+	public static bool isOrdinal(this DIRECTION dir){
+		return Mathf.Abs((int)dir) == 3 || Mathf.Abs((int)dir) == 4;
+	}
+
+	/*
 	 * public static Vector3 Round(this Vector3 vec)
 	 * 
 	 * Extension method for Vector3
