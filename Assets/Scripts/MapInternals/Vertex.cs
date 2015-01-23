859d5f6329d21332b3504f054e704b1ca92ac0d4
eazama <eazama001@gmail.com>
2015-01-22 17:59:37 -0800

    Added Vertex Class. Probably going to be unused

---
+++ Assets/Scripts/MapInternals/Vertex.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoomVertex {
+
+	Vector3 position;
+	Dictionary<DIRECTION, RoomVertex> neighbors;
+
+	private RoomVertex(Vector3 position) {
+		this.position = position.Round();
+		neighbors = new Dictionary<DIRECTION, RoomVertex>();
+	}
+
+	
+}
