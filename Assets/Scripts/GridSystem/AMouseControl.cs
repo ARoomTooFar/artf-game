9b818c51b8ec4469710412d61eed3585bc4e2d05
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-01-26 06:12:12 -0800

    duplicated kim son's tilemap code, modifying it and commenting it for my own use

---
+++ Assets/Scripts/GridSystem/AMouseControl.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AMouseControl : MonoBehaviour {
+
+	// reference to tile map. used for getting grid square size and other tilemap things
+	ATileMap tilemap;
+
+	// current tile
+	Vector3 currTile;
+
+	// selecting objects
+	GameObject selectionCube;
+	GameObject currentObj;
+
+	// reference to a list of objects to instantiate
+	GameObject menu;
+
+	// select when mouse over
+	bool notselected;
+	string selectedObject;
+
+	// Initialize variables, setting booleans
+	void Awake () {
+		//get the tilemap component in the object this script is in
+		//this script contains informat
+		tilemap = GetComponent<ATileMap> ();
+		notselected = true;
+		selectionCube = Instantiate (Resources.Load("selectionCube")) as GameObject;
+		selectedObject = null;
+	}
+	
+	void Update () {
+		RayToScene ();
+	}
+
+	void RayToScene(){
+		// get world coordinates with respect to mouse position by raycast
+		Ray ray = Camera.mainCamera.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hitInfo;
+		
+		if (Physics.Raycast (ray, out hitInfo, Mathf.Infinity)) {
+			//if we currently have an object selected, and the left-mouse button has been clicked
+			if(selectedObject != null && Input.GetMouseButtonDown (0)){
+				//get the coordinates where the mouse has clicked in the world
+				//this coordinate will be used for an object's position
+				Vector3 obj_pos = Camera.mainCamera.ScreenToWorldPoint(Input.mousePosition);
+				//set the object's height to the plane's height (the ground)
+				obj_pos.y = tilemap.transform.position.y;
+				//place the selected object at the coordinate we've made
+				placeItems(selectedObject, obj_pos);
+			} else{
+
+				switch(hitInfo.collider.gameObject.name){
+					case "selectionCube(Clone)":
+						snap2grid(hitInfo.point.x, hitInfo.point.z);
+						break;
+					case "TileMap":
+						snap2grid(hitInfo.point.x, hitInfo.point.z);
+						break;
+					default: 
+						Debug.Log(hitInfo.collider.gameObject.name);
+						break;
+				}
+			}
+		} else {
+			//renderer.material.color = Color.red;
+		}
+	}
+
+	void snap2grid(float x, float z){
+		x = Mathf.RoundToInt( x / tilemap.tileSize );
+		z = Mathf.RoundToInt( z / tilemap.tileSize );
+		currTile.x = x;
+		currTile.z = z;
+		if(notselected) selectionCube.transform.position = currTile;
+		
+		if (Input.GetMouseButtonDown (0)) {
+			selectTile ();
+		}
+		
+		
+		if (Input.GetMouseButtonDown (1)) {
+			deselect();
+		}
+	}
+
+	void selectTile(){
+		notselected = false;
+		selectionCube.transform.position = currTile;
+		selectionCube.GetComponent<MeshRenderer> ().material.color = Color.blue;
+	}
+
+	void deselect(){
+		notselected = true;
+		selectionCube.GetComponent<MeshRenderer> ().material.color = Color.green;
+	}
+
+	void placeItems(string name, Vector3 position){
+		position.x = Mathf.RoundToInt( position.x / tilemap.tileSize );
+		position.z = Mathf.RoundToInt( position.z / tilemap.tileSize );
+		currentObj = Instantiate (Resources.Load(name), position, Quaternion.identity) as GameObject;
+	}
+}
---
+++ Assets/Scripts/GridSystem/ATileMap.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshCollider))]
+[RequireComponent(typeof(MeshRenderer))]
+[ExecuteInEditMode]
+
+public class ATileMap : MonoBehaviour {
+
+	public int grid_x;
+	public int grid_z;
+
+	public float tileSize = 1.0f;
+	
+	// booleans for housekeeping
+
+	// Use this for initialization
+	void Start () {
+		grid_x = 25;
+		grid_z = 25;
+		buildMesh();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// 
+	void buildMesh(){
+
+		// total values
+		int tiles_total = grid_x * grid_z;
+		int tri_total = tiles_total * 2;
+
+		// number of vertices in each x z rows and the total number of vertices
+		int vx = grid_x + 1;
+		int vz = grid_z + 1;
+		int vert_total = vx * vz;
+
+		// Initialization
+		Vector3[] vertices = new Vector3[4];
+		int[] triangles = new int[2 * 3];
+		Vector3[] normals = new Vector3[4];
+
+		vertices [0] = new Vector3 (0, 0, 0);
+		vertices [1] = new Vector3 (tileSize * vx, 0, 0);
+		vertices [2] = new Vector3 (0, 0, tileSize * vz);
+		vertices [3] = new Vector3 (tileSize * vx, 0, tileSize * vz);
+
+		triangles[0] = 0;
+		triangles[2] = 1;
+		triangles[1] = 2;
+		
+		triangles[3] = 1;
+		triangles[4] = 2;
+		triangles[5] = 3;
+
+		for (int i = 0; i < 4; ++i) {
+			normals[i] = Vector3.up;
+		}
+
+		/*
+		// Store all possible vertices
+		for(int z = 0; z < vz; z++){
+			for(int x = 0; x < vx; x++){
+				vertices[z * vx + x] = new Vector3(x * tileSize, 0, z * tileSize);
+				normals[z * vx + x] = Vector3.up;
+			}
+		}*/
+		// Debug.Log ("Vertices logged");
+
+		/*
+		// Index all the vertices in [triangles] array, draw triangles.
+		for(int z = 0; z < grid_z; z++){
+			for(int x = 0; x < grid_x; x++){
+				int offset = (z * grid_x + x) * 6;
+				// Debug.Log(offset + " " + z + " " + x + " " + vx + " " + vz);
+
+				triangles[offset] 	  =	z * vx + x +	  0;
+				triangles[offset + 2] = z * vx + x + vx + 1;
+				triangles[offset + 1] = z * vx + x + vx	   ;
+
+				triangles[offset + 3] = z * vx + x +	  0;
+				triangles[offset + 5] = z * vx + x +	  1;
+				triangles[offset + 4] = z * vx + x + vx + 1;
+			}
+		}*/
+
+		// Debug.Log ("Triangles logged");
+
+
+		Mesh mesh = new Mesh();
+
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.normals = normals;
+
+		MeshFilter mesh_filter = GetComponent<MeshFilter>();
+		MeshRenderer mesh_render = GetComponent<MeshRenderer>();
+		MeshCollider mesh_collider = GetComponent<MeshCollider>();
+
+		mesh_filter.mesh = mesh;
+		mesh_collider.sharedMesh = mesh;
+		// Debug.Log ("mesh built");
+	}
+
+	void drawSquare(Vector3 A, Vector3 B, Vector3 C, Vector3 D){
+		Gizmos.DrawLine (A, B);
+		Gizmos.DrawLine (B, C);
+		Gizmos.DrawLine (C, D);
+		Gizmos.DrawLine (D, A);
+	}
+
+}
