62407b6f476d4c0f599ad79d9cbe867103377490
eazama <eazama001@gmail.com>
2015-01-26 14:43:33 -0800

    Split Extensions into different files per class

--- Assets/Scripts/Extensions/ExtensionsVector3.cs
+++ Assets/Scripts/Extensions/ExtensionsVector3.cs
@@ -6,39 +6,6 @@ using System.Collections.Generic;
 public static class Extensions
 {
 	/*
-	 * Extension method for DIRECTION enum.
-	 * 
-	 * returns the direction opposite of the direction used to call this method.
-	 * ex: DIRECTION.North.Opposite() == DIRECTION.South
-	 * 
-	 */
-	public static DIRECTION Opposite(this DIRECTION dir){
-		return (DIRECTION)(-(int)dir);
-	}
-
-	/*
-	 * Extension method for DIRECTION enum.
-	 * 
-	 * Returns true if the direction is a cardinal direction (N S E W)
-	 * Returns false otherwise
-	 * 
-	 */
-	public static bool isCardinal(this DIRECTION dir){
-		return Mathf.Abs((int)dir) == 1 || Mathf.Abs((int)dir) == 2;
-	}
-
-	/*
-	 * Extension method for DIRECTION enum.
-	 * 
-	 * Returns true if the direction is an ordinal direction (NW NE SE SW)
-	 * Returns false otherwise
-	 * 
-	 */
-	public static bool isOrdinal(this DIRECTION dir){
-		return Mathf.Abs((int)dir) == 3 || Mathf.Abs((int)dir) == 4;
-	}
-
-	/*
 	 * public static Vector3 Round(this Vector3 vec)
 	 * 
 	 * Extension method for Vector3
