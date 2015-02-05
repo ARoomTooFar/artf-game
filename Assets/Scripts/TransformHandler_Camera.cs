a4be0d548d6fdec3a8eab27a66975320e3256903
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-02-05 02:20:37 -0800

    new system for handling item lists in folders. buttons are iterated through and assigned listeners in a for-loop.

--- Assets/Scripts/TransformHandler_Camera.cs
+++ Assets/Scripts/TransformHandler_Camera.cs
@@ -6,7 +6,8 @@ using System.Collections;
 //as zoom in/out, and move along the x and z axis using
 //the arrow keys. Also movement along x and y axis
 //via dragging.
-public class TransformHandler_Camera : MonoBehaviour {
+public class TransformHandler_Camera : MonoBehaviour
+{
 	//Base location for camera, limits of camera,
 	//boolean control for camera angle, and speed of
 	//camera movement, drag speed, and mouse location
@@ -15,78 +16,82 @@ public class TransformHandler_Camera : MonoBehaviour {
 	float baseY, baseX, baseZ, minY, maxY, maxX, maxZ, dx, dy;
 	bool isTopDown;
 //	bool isDragging;
-	Vector2 dragSpeed;
-	private Vector3 mouseLocation;
-	private float scrollSpeed;
+	public Vector2 dragSpeed;
+	public float scrollSpeed;
 	Camera cam;
 
-	void Start () {
-		cam = GameObject.Find("UICamera").camera;
-		cam.transform.rotation = Quaternion.Euler(45,45,0);
-		cam.transform.position = new Vector3(-5f,21f,2.5f);
+	void Start ()
+	{
+		cam = GameObject.Find ("UICamera").camera;
+		cam.transform.rotation = Quaternion.Euler (45, 45, 0);
+//		cam.transform.position = new Vector3 (-5f, 21f, 2.5f);
+
 		//base height
-		baseY = 15f;
-		//max height
+		baseX = -5f;
+		baseY = 21f;
+		baseZ = 2.5f;
+
 		maxY = 25f;
-		//min height
 		minY = 5f;
+
 		//Camera angle controller
 		isTopDown = false;
+
 		//Are we dragging camera?
 //		isDragging = false;
-//		dragSpeed.x = .5f;
-//		dragSpeed.y = .5f;
+		dragSpeed.x = 3f;
+		dragSpeed.y = 3;
 
 		scrollSpeed = 2f;
 
 	}
 
-	public void zoomCamIn(float speed){
+	public void zoomCamIn (float speed)
+	{
 		baseY += speed;
 		//So it won't go too high
-		if(baseY > maxY){
+		if (baseY > maxY) {
 			baseY = maxY;
 		}
 	}
 
-	public void zoomCamOut(float speed){
+	public void zoomCamOut (float speed)
+	{
 		baseY -= speed;
 		//So it won't go too low
-		if(baseY < minY){
+		if (baseY < minY) {
 			baseY = minY;
 		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		//Scroll wheel forward? Go up.
-		if(Input.GetAxis ("Mouse ScrollWheel") < 0){
-			zoomCamIn(scrollSpeed);
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+			zoomCamIn (scrollSpeed);
 		}
 		//Scroll wheel back? Go down.
-		if(Input.GetAxis ("Mouse ScrollWheel") > 0 ){
-			zoomCamOut(scrollSpeed);
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+			zoomCamOut (scrollSpeed);
 		}
 
 		//Do we switch the view?
-		if (Input.GetButtonDown ("SwitchView")) {
-			if(isTopDown) {
-				isTopDown = false;
-				cam.transform.rotation = Quaternion.Euler(45,-45,0);
-			}
-			else {
-				isTopDown = true;
-				cam.transform.rotation = Quaternion.Euler(90,0,0);
-			}
-		}
+//		if (Input.GetButtonDown ("SwitchView")) {
+//			if(isTopDown) {
+//				isTopDown = false;
+//				cam.transform.rotation = Quaternion.Euler(45,-45,0);
+//			}
+//			else {
+//				isTopDown = true;
+//				cam.transform.rotation = Quaternion.Euler(90,0,0);
+//			}
+//		}
 
 		//click and drag 
-		//note for Jim or Leland: It took me 2 hours 
-		//to realize I had to remove the baseY = transform.position.y
-		//to get the desired effect...Please count that :(
 		if (Input.GetMouseButton (1)) {
-			dx = Input.GetAxis("Mouse X") * dragSpeed.x;
-			dy = Input.GetAxis("Mouse Y") * dragSpeed.y;
+			dx = Input.GetAxis ("Mouse X") * dragSpeed.x;
+			dy = Input.GetAxis ("Mouse Y") * dragSpeed.y;
 			cam.transform.position -= cam.transform.right * dx + cam.transform.up * dy;
 			baseX = cam.transform.position.x;
 			baseZ = cam.transform.position.z;
@@ -104,20 +109,20 @@ public class TransformHandler_Camera : MonoBehaviour {
 		//Move forward
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			if (isTopDown) {
-					baseZ += .1f; 
+				baseZ += .1f; 
 			} else {
-					baseZ += .1f;
-					baseX -= .1f;
+				baseZ += .1f;
+				baseX -= .1f;
 			}
 		}
 
 		//Move backward
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			if (isTopDown) {
-					baseZ -= .1f;
+				baseZ -= .1f;
 			} else {
-					baseZ -= .1f;
-					baseX += .1f;
+				baseZ -= .1f;
+				baseX += .1f;
 
 			}
 		}
@@ -125,25 +130,25 @@ public class TransformHandler_Camera : MonoBehaviour {
 		//Move left
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			if (isTopDown) {
-					baseX -= .1f;
+				baseX -= .1f;
 			} else {
-					baseZ -= .1f;
-					baseX -= .1f;
+				baseZ -= .1f;
+				baseX -= .1f;
 			}
 		}
 
 		//Move right
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			if (isTopDown) {
-					baseX += .1f;
+				baseX += .1f;
 			} else {
-					baseZ += .1f; 
-					baseX += .1f;
+				baseZ += .1f; 
+				baseX += .1f;
 			}
 		}
 
 		//Update camera position.
-		//		cam.transform.position = new Vector3 (baseX, baseY, baseZ);
+		cam.transform.position = new Vector3 (baseX, baseY, baseZ);
 				
 	}
 }
--- Assets/Scripts/UIHandler_FolderItems.cs
+++ Assets/Scripts/UIHandler_FolderItems.cs
@@ -5,28 +5,54 @@ using System.IO;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class UIHandler_FolderItems : MonoBehaviour {
+public class UIHandler_FolderItems : MonoBehaviour
+{
 
-	public Button Button_Dino; 
+//	Button Button_Dino; 
 
-	public MouseHandler_TileSelection mouseControl;
+	public GameObject itemList;
+	public Button[] buttonList;
+	public int numberOfButtons;
+	MouseHandler_TileSelection mouseControl;
 
 	// Use this for initialization
-	void Start () {
-		Button_Dino = this.gameObject.GetComponent("Button") as Button;
+	void Start ()
+	{
+		//get number of items in scrollview and make an array for them
+		numberOfButtons = itemList.gameObject.transform.GetChildCount ();
+		buttonList = new Button[numberOfButtons];
+		
+		//add all the items to their array
+		for (int i = 0; i < numberOfButtons; i++) {
+			buttonList [i] = itemList.gameObject.transform.GetChild (i).gameObject.GetComponent ("Button") as Button;
+		}
 
-		mouseControl = GameObject.Find("TileMap").GetComponent("MouseHandler_TileSelection") as MouseHandler_TileSelection;
+//		Button_Dino = this.gameObject.GetComponent("Button") as Button;
 
-		Button_Dino.onClick.AddListener (() => {
-			setSelectedObject ("Prefabs/dino"); });
+		mouseControl = GameObject.Find ("TileMap").GetComponent ("MouseHandler_TileSelection") as MouseHandler_TileSelection;
+
+		for (int i = 0; i < numberOfButtons; i++) {
+
+			//have to construct this string outside the button listener declaration
+			//below, or it will return a strange looking IndexArrayOutOfBoundsException
+			string g = "Prefabs/" + buttonList [i].transform.gameObject.name;
+
+			buttonList [i].onClick.AddListener (() => {
+				setSelectedObject (g); });
+		}
+
+//		Button_Dino.onClick.AddListener (() => {
+//			setSelectedObject ("Prefabs/dino"); });
 	}
 
-	void setSelectedObject(string s){
-		mouseControl.setSelectedObject(s);
+	void setSelectedObject (string s)
+	{
+		mouseControl.setSelectedObject (s);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+
 	}
 }
