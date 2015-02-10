c467ae2d295239c2b7999409dd227f17ed544e2d
calvinatlan <calvinatlan@gmail.com>
2015-02-09 19:47:31 -0800

    Aggro Table Fully Implemented
    All aggro table functions implemented and tested
    
    (Sorry for working in master, I lost track of what branch I was on and
    it was too late to switch)

--- Assets/1.Scripts/Units/Enemies/TableTester.cs
+++ Assets/1.Scripts/Units/Enemies/TableTester.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class TableTester : MonoBehaviour {
 
 	private AggroTable test;
-	private GameObject p1;
-	private GameObject p2;
-	private GameObject p3;
-	private GameObject p4;
+	public GameObject p1;
+	public GameObject p2;
+	public GameObject p3;
+	public GameObject p4;
+	public bool testing = false;
 
 	// Use this for initialization
 	void Start () 
 	{
 		test = new AggroTable ();
+		test.testing = testing;
 		test.add (p1, 10);
 		test.add (p2, 4);
 		test.add (p3, 21);
 		test.add (p4, 14);
-		test.add (p3, 20);
+		test.add (p4, 1);
+		test.add (p1, 11);
+		test.add (p2, 25);
 		test.printOrder ();
 	}
 	
