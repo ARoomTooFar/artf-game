753353cb3c8d7c182d19778db5360921b8d0b97e
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-02-24 12:46:55 -0800

    commit before merging from master

--- Assets/Scripts/Renderer_TileMapGraphics.cs
+++ Assets/Scripts/Renderer_TileMapGraphics.cs
@@ -7,11 +7,13 @@ public class Renderer_TileMapGraphics : MonoBehaviour
 {
 	Input_TileMap input_tileMap;
 	public Material selectionMat; //material for selected tiles
+	TileMap tileMap;
 //	HashSet<Vector3> selectedTiles;
 	
 	void Awake ()
 	{
 //		selectedTiles = new HashSet<Vector3> ();
+		tileMap = GameObject.Find ("TileMap").GetComponent("TileMap") as TileMap;
 		input_tileMap = GameObject.Find ("TileMap").GetComponent ("Input_TileMap") as Input_TileMap;
 	}
 
@@ -40,32 +42,33 @@ public class Renderer_TileMapGraphics : MonoBehaviour
 	}
 
 	/* draw the grid lines */
-//	void drawGrid(){
-//		GL.Begin (GL.LINES);
+	void drawGrid(){
+		GL.Begin (GL.LINES);
 //		gridMat.SetPass (0);
-//
-//		/* get size of tile map */
-//		int size_x = 0; int size_z = 0;
+		selectionMat.SetPass (0);
+
+		/* get size of tile map */
+		int size_x = 0; int size_z = 0;
 //		if (tiles != null) {
-//			size_x = tiles.GetComponent<TileMap>().grid_x;
-//			size_z = tiles.GetComponent<TileMap>().grid_z;
+			size_x = tileMap.grid_x;
+			size_z = tileMap.grid_z;
 //		}
-//
-//		/* draw grid */
-//		int count = 0;
-//		for (int z = 0; z < size_z; z++) {
-//			if(count == 5){
-//				
-//			}
-//			GL.Vertex (new Vector3 (0f, 0f, z));
-//			GL.Vertex (new Vector3 (size_x, 0f, z));
-//		}
-//		
-//		for (int x = 0; x < size_x; x++) {
-//			GL.Vertex (new Vector3 (x, 0f, 0f));
-//			GL.Vertex (new Vector3 (x, 0f, size_z - 0f));
-//		}
-//		
-//		GL.End ();
-//	}
+
+		/* draw grid */
+		int count = 0;
+		for (int z = 0; z < size_z; z++) {
+			if(count == 5){
+				
+			}
+			GL.Vertex (new Vector3 (0f, 0f, z));
+			GL.Vertex (new Vector3 (size_x, 0f, z));
+		}
+		
+		for (int x = 0; x < size_x; x++) {
+			GL.Vertex (new Vector3 (x, 0f, 0f));
+			GL.Vertex (new Vector3 (x, 0f, size_z - 0f));
+		}
+		
+		GL.End ();
+	}
 }
