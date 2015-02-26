f19d1f8bae82c8f031d00d0d81fd7b50c2af6eec
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-02-25 23:31:21 -0800

    infinite grid done. kind of choppy now when moving camera. not sure exactly what to alter to make it smooth again, or if it's even possible.

--- Assets/Scripts/Renderer_TileMapGraphics.cs
+++ Assets/Scripts/Renderer_TileMapGraphics.cs
@@ -78,24 +78,19 @@ public class Renderer_TileMapGraphics : MonoBehaviour
 		float tileMapSizeZ = tileMap.grid_z * tileMapGameObj.transform.localScale.z;
 
 
-//		Debug.Log(tileMap.grid_z * tileMapGameObj.transform.localScale.z);
-
-		for (int z = 0; z < tileMapSizeZ; z++) {
-			GL.Vertex(new Vector3(Mathf.Floor()));
+		//draw grid over tilemap
+		for (int z = (int)Mathf.Floor(zLowerBound); z < (int)Mathf.Floor(zUpperBound); z++) {
+			GL.Vertex(new Vector3(Mathf.Floor(xLowerBound), 0f, z + 0.5f));
+			GL.Vertex(new Vector3(Mathf.Floor(xUpperBound), 0f, z + 0.5f));
 		}
-
-		for (int z = 0; z < size_z; z++) {
-			GL.Vertex (new Vector3 (0f - 1, 0f, z + 0.5f));
-			GL.Vertex (new Vector3 (size_x - 1, 0f, z + 0.5f));
-		}
-		
-		for (int x = 0; x < size_x; x++) {
-			GL.Vertex (new Vector3 (x - 0.5f, 0f, 0f));
-			GL.Vertex (new Vector3 (x - 0.5f, 0f, size_z - 0f));
+		for (int x = (int)Mathf.Floor(xLowerBound); x < (int)Mathf.Floor(xUpperBound); x++) {
+			GL.Vertex(new Vector3(x - 0.5f, 0f, Mathf.Floor(zLowerBound)));
+			GL.Vertex(new Vector3(x - 0.5f, 0f, Mathf.Floor(zUpperBound)));
 		}
 
 
 
+		//old non-infinite way
 		/* draw grid */
 //		int count = 0;
 //		for (int z = 0; z < size_z; z++) {
