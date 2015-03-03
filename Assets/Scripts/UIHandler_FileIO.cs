b246bbab11189c0efda5ce2ec61aa5eb8ff584fb
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-03-03 05:52:02 -0800

    Got loading to work with downloading and saving

--- Assets/Scripts/UIHandler_FileIO.cs
+++ Assets/Scripts/UIHandler_FileIO.cs
@@ -21,63 +21,77 @@ public class UIHandler_FileIO : MonoBehaviour
 	static ItemClass itemClass;
 
 	private Farts serv;
-	long levelId;
-	string levelTHING;
-	private WWW lvlUpdate; // WWW request to server for level updating
+    private string lvlData = "";
+    private WWW udLvlReq = null; // WWW request to server for level updating
+
+    public string lvlId = "0";
+    public Text txtDlLvl;
+    public Text txtUdLvl;
 
 	IEnumerator Start ()
 	{
+        txtUdLvl.enabled = false;
+
 		itemClass = new ItemClass ();
 		
 		output_tileMap = GameObject.Find ("TileMap").GetComponent ("Output_TileMap") as Output_TileMap;
 		
-		serv = gameObject.AddComponent<Farts> ();
+        //loadingObj = gameObject.AddComponent<LoadingObj>();
 		Button_Save.onClick.AddListener (() => {
 			saveFile (); });
 //		Button_Load.onClick.AddListener (() => {
 //			loadFile ();});
-		
 
-		// Download level example
-		WWW dlLvlReq = serv.getLvlWww("levelID");
-		yield return dlLvlReq;
-		
-		// Use the downloaded level data
-		string dlLvlData = dlLvlReq.text;
-		
-		// Check if level data is valid
-		if (serv.dataCheck(dlLvlData))
-		{
-			Debug.Log("DOWNLOAD SUCCEEDED: " + dlLvlData);
-			MapDataParser.ParseSaveString(dlLvlData);
-		}
-		else
-		{
-			Debug.Log("ERROR: LEVEL DATA DOWNLOAD FAILED");
-		}
+        serv = gameObject.AddComponent<Farts>();
+
+        // Need to get the level ID from browser, but for now we're cheezin it
+        lvlId = "5684666375864320";
+
+        // Download the level
+        WWW dlLvlReq = serv.getLvlWww(lvlId);
+        yield return dlLvlReq;
+
+        // Use the downloaded level data
+        lvlData = dlLvlReq.text;
+        Debug.Log(lvlData);
+        if (serv.dataCheck(lvlData))
+        {
+            txtDlLvl.enabled = false;
+            MapDataParser.ParseSaveString(lvlData);
+            Debug.Log("LVL DL SUCCESS: " + lvlData);
+        }
+        else
+        {
+            txtDlLvl.text = "LVL DL ERROR: Level ID doesn't exist.";
+            Debug.Log("ERROR: Level download failed. " + lvlId + " doesn't exist.");
+        }
 	}
 	
 	public void saveFile ()
 	{
-		lvlUpdate = serv.updateLvl("levelID", "gameaccountID", MapData.SaveString, "draftleveldata");
-	}
-	
+        udLvlReq = serv.updateLvl("5684666375864320", "123", MapData.SaveString, "draftleveldataisbutts");
+        txtUdLvl.enabled = true;
+	}	
 
 	void Update()
 	{
+        // Wait for download to complete
+
 		// Use the returned data from update level request's coroutine
-		if (lvlUpdate != null)
+        if (udLvlReq != null)
 		{
-			if (lvlUpdate.isDone && lvlUpdate.error == null)
+            if (udLvlReq.isDone && udLvlReq.error == null)
+            {
+                txtUdLvl.enabled = false;
+                Debug.Log("UPDATE SUCCEEDED: " + udLvlReq.text);
+                udLvlReq = null; // Reset lvlUpdate to null so it doesn't the Debug.Log doesn't spam the console every 
+            }
+            else if (udLvlReq.error != null)
 			{
-				Debug.Log("UPDATE SUCCEEDED: " + lvlUpdate.text);
-				lvlUpdate = null; // Reset lvlUpdate to null so it doesn't the Debug.Log doesn't spam the console every tick
-			}
-			else if (lvlUpdate.error != null)
-			{
-				Debug.Log("ERROR: LEVEL UPDATE FAILED");
+                txtUdLvl.text = "LVL UD ERROR: Level update failed.";
+                Debug.Log("ERROR: LEVEL UPDATE FAILED");
+                udLvlReq = null;
 			}
 		}
 	}
-	
 }
