c448ef27de6eed64816c4b4d87f20ef99737fba1
Sonny <kiqnguye@ucsc.edu>
2015-03-05 13:32:57 -0800

    bushman in progress

---
+++ Assets/1.Scripts/Units/Enemies/Bushman.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bushman : MobileEnemy {
+
+	bool inFrenzy;
+	float frenzy_counter;
+	float frenzy_growth;
+	
+	protected override void Awake () {
+		base.Awake ();
+		inFrenzy = false;
+		frenzy_counter = 0;
+		frenzy_growth = 2;
+	}
+	
+	protected override void Start() {
+		base.Start ();
+	}
+	
+	protected override void Update() {
+
+	}
+	
+	protected override void setInitValues() {
+		base.setInitValues();
+		stats.maxHealth = 140;
+		stats.health = stats.maxHealth;
+		stats.armor = 0;
+		stats.strength = 10;
+		stats.coordination=0;
+		stats.speed=9;
+		stats.luck=0;
+		
+		this.minAtkRadius = 0.0f;
+		this.maxAtkRadius = 5.0f;
+	}
+
+	/*
+	protected bool isFlared(){
+		return flared;
+	}*/
+
+	protected bool isFrenzy(){
+		return inFrenzy;
+	}
+
+	/*
+	protected bool isFlared(Character a){
+		Bushman bushman = (Bushman) a;
+		return bushman.isFlared();
+	}*/
+
+	protected bool isFrenzy(Character a){
+		Bushman bushman = (Bushman) a;
+		return bushman.isFrenzy ();
+	}
+
+	protected void Frenzy(){
+
+	}
+}
---
+++ Assets/1.Scripts/Units/Misc Classes/BuffsAndDebuffs/BuffsDebuffs/Override/Frenzy.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Frenzy : Override {
+
+	private float spd;
+	private float str;
+	private float def;
+	float threshold;
+	int level;
+
+	public Frenzy () {
+		level = 1;
+		name = "Frenzy";
+		spd = 100;
+		str = 100;
+		def = 100;
+	}
+
+	public void setSpeedBoost(float percent){
+		spd = percent;
+	}
+
+	public void setStrBoost(float percent){
+		str = percent;
+	}
+
+	public void setDefBoost(float percent){
+		def = percent;
+	}
+
+	public void FrenzyUp(){
+
+	}
+
+	public void reset(){
+
+	}
+
+	protected override void bdEffects(BDData newData){
+		base.bdEffects (newData);
+		newData.unit.stats.dmgManip.setDamageReduction(0, def);
+		newData.unit.stats.spdManip.setSpeedAmplification(spd);
+		newData.unit.stats.dmgManip.setDamageAmplification(0, str);
+	}
+
+	protected override void removeEffects (BDData oldData, GameObject source) {
+		base.removeEffects (oldData, source);
+		oldData.unit.stats.dmgManip.removeDamageReduction(0, def);
+		oldData.unit.stats.spdManip.removeSpeedAmplification(spd);
+		oldData.unit.stats.dmgManip.removeDamageAmplification(0, str);
+	}
+
+	public override void purgeBD(Character unit, GameObject source) {
+		base.purgeBD (unit, source);
+	}
+}
