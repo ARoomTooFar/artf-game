1ffaa5769e9e3e20c501f3ed8756bd262bcf20e1
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-03-14 06:10:25 -0700

    Cool new title screen

---
+++ Assets/1.Scripts/GSManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GSManager : MonoBehaviour {
+    public float health;
+    public float experience;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
