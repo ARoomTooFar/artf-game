f3fa0fda2fae3c8e7d1a1d3b3530f5ae80a8fe99
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-03-14 09:24:12 -0700

    Ensures GSManager is a singleton

--- Assets/1.Scripts/GSManager.cs
+++ Assets/1.Scripts/GSManager.cs
@@ -2,11 +2,22 @@
 using System.Collections;
 
 public class GSManager : MonoBehaviour {
+    // persistent data
+    public static GSManager gsManager;
     public float health;
     public float experience;
 
-	void Start () {
-        DontDestroyOnLoad(gameObject);  // don't destroy itself between scene loads
+	void Awake () {
+        // ensure gsManager is a singleton
+        if (gsManager == null)
+        {
+            DontDestroyOnLoad(gameObject);  // don't destroy itself between scene loads
+            gsManager = this;
+        }
+        else if (gsManager != this)
+        {
+            Destroy(gameObject);
+        }
 	}
 	
     public void LoadScene (int scene)
