8e3953bf4e3354750f16980cdaf623cd6c6f311a
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-03-14 09:44:37 -0700

    Loading bar shows progress while loading

--- Assets/1.Scripts/Screens/Title/TitleHand.cs
+++ Assets/1.Scripts/Screens/Title/TitleHand.cs
@@ -7,19 +7,25 @@ public class TitleHand : MonoBehaviour {
     private GSManager gsManager;
     private Button btnStartGame;
     private GameObject loadingBG;
+    private Slider loadingBar;
+
+    private AsyncOperation loadingProgress;
 
 	void Start ()
     {
         gsManager = GameObject.Find("GSManager").GetComponent<GSManager>();
         btnStartGame = GameObject.Find("BtnStartGame").GetComponent<Button>();
+
         loadingBG = GameObject.Find("LoadingBG");
+        loadingBar = GameObject.Find("LoadingBar").GetComponent<Slider>();
 
         loadingBG.SetActive(false);  // hide LoadingBG. can't hide in inspector because Find() can't find hidden objects.
 
         // add functions to be called when buttons are clicked
         btnStartGame.onClick.AddListener(() =>
             {
-                StartGame();
+                loadSceneAsync(14);
+                //StartGame();
             }
         );
 	}
@@ -28,5 +34,22 @@ public class TitleHand : MonoBehaviour {
     {
         loadingBG.SetActive(true);
         gsManager.LoadScene(13);
+    }
+
+    void loadSceneAsync(int scene)
+    {
+        loadingBG.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(scene));
+    }
+
+    IEnumerator LoadLevelWithBar(int scene)
+    {
+        loadingProgress = Application.LoadLevelAsync(scene);
+
+        while (!loadingProgress.isDone)
+        {
+            loadingBar.value = loadingProgress.progress;
+            yield return null;
+        }
     }
 }
