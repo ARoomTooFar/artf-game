7e2de0181b28e5fd0d925eff64316e7e5e1a81f9
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-03-15 00:24:45 -0700

    Creates DemoHand for UI_Example

---
+++ Assets/1.Scripts/Screens/DemoHand.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DemoHand : MonoBehaviour {
+	private GSManager gsManager;
+	private Button btnInsertCoin;
+	private GameObject loadingBG;
+	
+	void Start ()
+	{
+		gsManager = GameObject.Find("GSManager").GetComponent<GSManager>();
+		btnInsertCoin = GameObject.Find("BtnInsertCoin").GetComponent<Button>();
+		
+		// hide LoadingBG. can't hide in inspector because Find() can't find hidden objects.
+		loadingBG = GameObject.Find("LoadingBG");
+		loadingBG.SetActive(false);
+		
+		// add functions to be called when buttons are clicked
+		btnInsertCoin.onClick.AddListener(() =>
+			{
+				ZoomToLevelSelect ();
+			}
+		);
+	}
+
+	void ZoomToLevelSelect ()
+	{
+		Debug.Log ("hyeeeaaachaaaa");
+	}
+}
