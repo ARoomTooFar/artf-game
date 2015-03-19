27ed5753d2ec5de733618d2f6bb483a71b322c04
eazama <eazama001@gmail.com>
2015-03-19 12:03:50 -0700

    Removing MapInternalsV2
    I'm just going to clean up my original code

--- Assets/Scripts/MapInternalsV2/BorderEntity.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class BorderEntity : MonoBehaviour {
-	public string BlockID;
-
-	public GameObject obj;
-}
