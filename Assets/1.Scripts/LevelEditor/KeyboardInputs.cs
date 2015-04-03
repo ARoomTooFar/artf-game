b20b155d7bd270301cd28e0b7d096fd48ef900b9
eazama <eazama001@gmail.com>
2015-04-02 18:30:51 -0700

    Code Organization

--- Assets/1.Scripts/LevelEditor/KeyboardInputs.cs
+++ Assets/1.Scripts/LevelEditor/KeyboardInputs.cs
@@ -7,5 +7,21 @@ public class KeyboardInputs : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Backspace)) {
 			MapData.removeObjects((GameObject.Find ("TileMap").GetComponent("TileMapController") as TileMapController).selectedTiles);
 		}
+
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			GameObject.Find("UICamera").GetComponent<CameraMovement>().moveForward();
+		}
+
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			GameObject.Find("UICamera").GetComponent<CameraMovement>().moveBackward();
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			GameObject.Find("UICamera").GetComponent<CameraMovement>().moveLeft();
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			GameObject.Find("UICamera").GetComponent<CameraMovement>().moveRight();
+		}
 	}
 }
