d7305ffbffebf24b7529c948eb15e5d7c3c6947d
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-04-02 20:05:28 -0700

    stupid mode button highlighting

--- Assets/1.Scripts/LevelEditor/Mode.cs
+++ Assets/1.Scripts/LevelEditor/Mode.cs
@@ -6,18 +6,45 @@ using System.Collections;
  * 
  */
 
-public class Mode : MonoBehaviour {
-	public bool Room = false;
-	public bool Tile = true;
+public static class Mode
+{
+	static bool roomMode = false;
+	static bool tileMode = true;
 
-	public void SetRoomMode(){
-		Room = true;
-		Tile = false;
+	static GameObject Button_TileMode_Background = GameObject.Find("Button_TileMode_Background");
+	static GameObject Button_RoomMode_Background = GameObject.Find("Button_RoomMode_Background");
+
+	static Mode(){
+		Button_TileMode_Background = GameObject.Find("Button_TileMode_Background");
+		Button_RoomMode_Background = GameObject.Find("Button_RoomMode_Background");
 	}
 
-	public void SetTileMode(){
-		Room = false;
-		Tile = true;
+	public static void setRoomMode ()
+	{
+		roomMode = true;
+		tileMode = false;
+
+		Button_RoomMode_Background.SetActive(true);
+		Button_TileMode_Background.SetActive(false);
+	}
+
+	public  static void setTileMode ()
+	{
+		roomMode = false;
+		tileMode = true;
+
+		Button_RoomMode_Background.SetActive(false);
+		Button_TileMode_Background.SetActive(true);
+	}
+
+	public  static bool isTileMode ()
+	{
+		return tileMode;
+	}
+
+	public  static bool isRoomMode ()
+	{
+		return roomMode;
 	}
 
 }
