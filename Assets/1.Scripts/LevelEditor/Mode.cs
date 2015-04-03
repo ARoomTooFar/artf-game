2e7426922f866d1a322282a28a9eeef772d9d551
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-04-02 18:42:00 -0700

    mode stuff

---
+++ Assets/1.Scripts/LevelEditor/Mode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Controls the mode of the level editor
+ * 
+ */
+
+public class Mode : MonoBehaviour {
+	public bool Room = false;
+	public bool Tile = true;
+
+	public void SetRoomMode(){
+		Room = true;
+		Tile = false;
+	}
+
+	public void SetTileMode(){
+		Room = false;
+		Tile = true;
+	}
+
+}
