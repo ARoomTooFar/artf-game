b0c57f9c625385aa2eef18ca6f9f285fc128ad3f
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-04-05 02:59:49 -0700

    Adds more buttons in level select

--- Assets/1.Scripts/Screens/Title/TitleHand.cs
+++ Assets/1.Scripts/Screens/Title/TitleHand.cs
@@ -12,33 +12,62 @@ public class TitleHand : MonoBehaviour {
 
     /* LEVEL SELECT */
     private GameObject btnLevel1;
+	private GameObject btnLevel2;
+	private GameObject btnLevel3;
 
 	void Start ()
     {  
 		gsManager = GameObject.Find("GSManager").GetComponent<GSManager>();
         btnStartGame = GameObject.Find("BtnStartGame").GetComponent<Button>();
         btnLevel1 = GameObject.Find("BtnLevel1");
+		btnLevel2 = GameObject.Find("BtnLevel2");
+		btnLevel3 = GameObject.Find("BtnLevel3");
 
 		// hide LoadingBG. can't hide in inspector because Find() can't find hidden objects.
         loadingBG = GameObject.Find("LoadingBG");
         loadingBG.SetActive(false);
 
         // hide level select
-        btnLevel1.SetActive(false);
+		hideLevelSelect ();
 
         // add functions to be called when buttons are clicked
         btnStartGame.onClick.AddListener(() =>
             {
-                btnLevel1.SetActive(true);
+				showLevelSelect();
             }
         );
 
         btnLevel1.GetComponent<Button>().onClick.AddListener(() =>
             {
-				btnLevel1.SetActive (false);
+				hideLevelSelect ();
 				gsManager.LoadLevel("5713573250596864");
-				//gsManager.LoadLevel("4876504257265664");
 			}
 		);
+
+		btnLevel2.GetComponent<Button>().onClick.AddListener(() =>
+		    {
+				hideLevelSelect ();
+				gsManager.LoadLevel("4876504257265664");
+			}
+		);
+
+		btnLevel3.GetComponent<Button>().onClick.AddListener(() =>
+			{
+				hideLevelSelect ();
+				gsManager.LoadLevel("4851447149625344");
+			}
+		);
+	}
+
+	void hideLevelSelect () {
+		btnLevel1.SetActive(false);
+		btnLevel2.SetActive(false);
+		btnLevel3.SetActive(false);
+	}
+
+	void showLevelSelect () {
+		btnLevel1.SetActive(true);
+		btnLevel2.SetActive(true);
+		btnLevel3.SetActive(true);
 	}
 }
