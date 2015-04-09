3da9df169883a786e0b8d1418daa0dec6dddcbfb
Son <kiqnguye@ucsc.edu>
2015-04-09 02:28:47 -0700

    Bushman frenzy now easier to modify value

--- Assets/1.Scripts/Units/Enemies/Mobile/Bushman.cs
+++ Assets/1.Scripts/Units/Enemies/Mobile/Bushman.cs
@@ -7,12 +7,25 @@ public class Bushman : MobileEnemy {
 
 
 	private class BMFrenzyGrowth : StatsMultiplier {
-		
-		protected StatsMultiplier FrenzyGrowth(float dmgUp, float dmgRedUp, float spdUp, float currentGrowth) {
+		public float basedmg, currentdmg, dmgGrowth;
+		public float basedef, currentdef, defGrowth;
+		public float basespd, currentspd, spdGrowth;
+
+		// variable growth
+		public StatsMultiplier FrenzyGrowth(float dmgUp, float dmgRedUp, float spdUp) {
 			StatsMultiplier nextLv = new StatsMultiplier ();
-			nextLv.dmgAmp = dmgUp * currentGrowth;
-			nextLv.dmgRed = dmgRedUp * currentGrowth;
-			nextLv.speed = spdUp * currentGrowth;
+			nextLv.dmgAmp = basedmg + dmgUp * dmgGrowth;
+			nextLv.dmgRed = basedef + dmgRedUp * defGrowth;
+			nextLv.speed = basespd + spdUp * spdGrowth;
+			return nextLv;
+		}
+
+		// constant growth
+		public StatsMultiplier CFrenzyGrowth(int currentStage){
+			StatsMultiplier nextLv = new StatsMultiplier ();
+			nextLv.dmgAmp = basedmg + currentStage * dmgGrowth;
+			nextLv.dmgRed = basedef + currentStage * defGrowth;
+			nextLv.speed = basespd + currentStage * spdGrowth;
 			return nextLv;
 		}
 	}
@@ -164,20 +177,27 @@ public class Bushman : MobileEnemy {
 
 	protected void setFrenzy() {
 		powlvs = new PowerLevels (this);
-		StatsMultiplier stage0 = new StatsMultiplier (); stage0.dmgAmp = 0f; stage0.dmgRed = 0f; stage0.speed = 0f;
+/*		StatsMultiplier stage0 = new StatsMultiplier (); stage0.dmgAmp = 0f; stage0.dmgRed = 0f; stage0.speed = 0f;
 		StatsMultiplier stage1 = new StatsMultiplier (); stage1.dmgAmp = 0.2f; stage1.dmgRed = -0.1f; stage1.speed = 0.1f;
 		StatsMultiplier stage2 = new StatsMultiplier (); stage2.dmgAmp = 0.3f; stage2.dmgRed = -0.25f; stage2.speed = 0.2f;
 		StatsMultiplier stage3 = new StatsMultiplier (); stage3.dmgAmp = 0.36f; stage3.dmgRed = -0.3f; stage3.speed = 0.25f;
 		StatsMultiplier stage4 = new StatsMultiplier (); stage4.dmgAmp = 0.5f; stage4.dmgRed = -0.35f; stage4.speed = 0.4f;
 		StatsMultiplier stage5 = new StatsMultiplier (); stage5.dmgAmp = 0.75f; stage5.dmgRed = -0.4f; stage5.speed = 0.6f;
-		StatsMultiplier stage6 = new StatsMultiplier (); stage6.dmgAmp = 1.0f; stage6.dmgRed = -0.8f; stage6.speed = 1.2f;
-		powlvs.addStage(stage0, 0);
-		powlvs.addStage(stage1, 10);
-		powlvs.addStage(stage2, 20);
-		powlvs.addStage(stage3, 25);
-		powlvs.addStage(stage4, 36);
-		powlvs.addStage(stage5, 45);
-		powlvs.addStage(stage6, 60);
+		StatsMultiplier stage6 = new StatsMultiplier (); stage6.dmgAmp = 1.0f; stage6.dmgRed = -0.8f; stage6.speed = 1.2f;*/
+
+		BMFrenzyGrowth bmf = new BMFrenzyGrowth ();
+		bmf.basedmg = 0f; bmf.basedef = 0f; bmf.basespd = 0f;
+		bmf.dmgGrowth = 0.1f; bmf.defGrowth = -0.1f; bmf.spdGrowth = 0.1f;
+
+		powlvs.addStage(bmf.FrenzyGrowth(0f, 0f, 0f), 0);
+		powlvs.addStage(bmf.FrenzyGrowth(2f, 1f, 1f), 10);
+		powlvs.addStage(bmf.FrenzyGrowth(3f, 2.5f, 2f), 20);
+		powlvs.addStage(bmf.FrenzyGrowth(3.6f, 3f, 2.5f), 25);
+		powlvs.addStage(bmf.FrenzyGrowth(5f, 3.5f, 4f), 36);
+		powlvs.addStage(bmf.FrenzyGrowth(7.5f, 8f, 6f), 45);
+		powlvs.addStage(bmf.FrenzyGrowth(10f, 8f, 12f), 60);
+
+
 	}
 
 	/*
