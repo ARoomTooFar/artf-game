27158f03cb0ed53432fe78b09046782015f2c040
Sunny <kiqnguye@ucsc.edu>
2015-04-10 00:41:56 -0700

    cablevine behaviors

--- Assets/1.Scripts/Units/Enemies/Stationary/CableVine.cs
+++ Assets/1.Scripts/Units/Enemies/Stationary/CableVine.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 using System.Collections;
 
 public class CableVine : StationaryEnemy {
-
-	public Stun stunObj;
-	public GenericDoT constrict;
+	
+	protected Stun stun;
+	protected GenericDoT constrict;
 	bool inStealth;
 	HingeJoint tether;
 	Rigidbody joints;
+	protected Transform origin;
 //	CableMaw MyMum;
 
-	// Use this for initialization
 	protected override void Awake () {
 		base.Awake();
+	}
+
+	protected override void Start() {
+		base.Start ();
+		stun = new Stun ();
+		constrict = new GenericDoT (10);
 	}
 	
 	// Update is called once per frame
