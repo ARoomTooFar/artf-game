cfc1c6364c382de479b360f1af926f2fc2219abf
Paul KasumDragonSlayer <keerothezero@gmail.com>
2015-04-10 14:05:17 -0700

    LobItem, made heal shot and net shot use lob

--- Assets/1.Scripts/Placements/Explosion.cs
+++ Assets/1.Scripts/Placements/Explosion.cs
@@ -5,18 +5,21 @@ public class Explosion : MonoBehaviour {
 	public float lifetime;
 	public bool charDeath;
 	public GameObject grave;
-	public GameObject lootDrop;
+	public LootDrop lootDrop;
 	public Character unit;
 	protected Quaternion spray;
 	protected float variance;
-	protected Transform localTarget;
+	protected Transform localTarget,savedSpot;
 	//protected 
 	// Use this for initialization
 	void Start () {
 		variance = 45f;
 		localTarget = this.transform;
+		localTarget.position = new Vector3(localTarget.position.x,localTarget.position.y+5,localTarget.position.z);
+		savedSpot = localTarget;
+		if(unit!=null){
 		spray = Quaternion.Euler(new Vector3(unit.transform.eulerAngles.x,Random.Range(-variance+unit.transform.eulerAngles.y,variance+unit.transform.eulerAngles.y),unit.transform.eulerAngles.z));
-		
+		}
 		lifetime = 2f;
 		StartCoroutine(Wait(lifetime-.25f));
 	}
@@ -28,31 +31,33 @@ public class Explosion : MonoBehaviour {
 	void Update () {
 		Destroy (gameObject, lifetime);
 	}
-	public void lootBoom(){
+	/*public void lootBoom(){
 		if(unit.drop !=null){
 			for(int i = 0; i<4; i++){//Four is replaceable
-				localTarget.position = Vector3.MoveTowards(transform.position, localTarget.position+localTarget.transform.forward*10, 999999);
-				lootDrop = (GameObject) Instantiate(unit.drop, localTarget.position, localTarget.rotation);
-				this.shoot (this.transform.AngledArcTrajectory(localTarget.transform.position, angle));
-				localTarget.rotation = spray;
+				localTarget.position = savedSpot.position;
+				spray = Quaternion.Euler(new Vector3(localTarget.transform.eulerAngles.x,-variance*2+localTarget.transform.eulerAngles.y,localTarget.transform.eulerAngles.z));
+				localTarget.rotation = spray;//
+				localTarget.position = Vector3.MoveTowards(localTarget.position, localTarget.position+localTarget.transform.forward*8, 10);
+				lootDrop = ((GameObject)Instantiate(unit.drop, unit.transform.position, unit.transform.rotation)).GetComponent<LootDrop>();
+				lootDrop.shoot (lootDrop.transform.AngledArcTrajectory(localTarget.transform.position,10));
+				spray = Quaternion.Euler(new Vector3(localTarget.transform.eulerAngles.x,-variance*2+localTarget.transform.eulerAngles.y,localTarget.transform.eulerAngles.z));
+				//localTarget.rotation = spray;//
 			}
 		}
-	}
+	}*/
+	
 	private IEnumerator Wait(float duration){
 		for (float timer = 0; timer < duration; timer += Time.deltaTime){
 			yield return 0;
 		}
 		if(charDeath){
 			if(unit.GetType() == typeof(Player)){
-				Grave g = ((GameObject)Instantiate(grave, unit.transform.position, grave.transform.rotation)).GetComponent<Grave>();
+				Grave g = ((GameObject)Instantiate(grave, this.transform.position, this.transform.rotation)).GetComponent<Grave>();
 				g.setInitValues((Player)unit);
-				lootBoom();
+				//lootBoom();
 			}
 			//Instantiate(grave,transform.position+grave.transform.position,transform.rotation);
 		}
 	}
 
-	protected virtual void shoot(Vector3 trajectory) {
-		this.GetComponent<Rigidbody> ().velocity = trajectory;
-	}
 }
