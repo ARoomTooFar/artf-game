fa3ef7d617a0283036b3022cfc23ef66262a0036
eazama <eazama001@gmail.com>
2015-04-15 17:00:59 -0700

    Tweaked initial rotation

--- Assets/1.Scripts/LevelEditor/Global.cs
+++ Assets/1.Scripts/LevelEditor/Global.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public static class Global {
 	public static readonly Vector3 nullVector3 = new Vector3(-1, -1, -1);
-	public static readonly Vector3 initCameraRotation = new Vector3(70,-45,0);
+	public static readonly Vector3 initCameraRotation = new Vector3(60,-30,0);
 	public static readonly Vector3 initCameraPosition = new Vector3(12.5f, 30f, -12.5f);
 	public static readonly float mouseDeadZone = 10f;
 	public static readonly int grid_x = 300;
