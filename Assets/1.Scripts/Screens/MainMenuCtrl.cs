1ddc3fa217a8d46fb9d972e26abae6a5b30fe047
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-04-18 23:29:47 -0700

    Progress on main menu UI

---
+++ Assets/1.Scripts/Screens/MainMenuCtrl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuCtrl : MonoBehaviour {
+    private int menuWidth = 1;
+    private int menuHeight = 2;
+
+    private GameObject[,] p1Menu;
+    private int p1LocX = 0;
+    private int p1LocY = 0;
+
+	void Start () {
+        // create p1Menu
+        p1Menu = new GameObject[menuHeight, menuWidth];
+
+        p1Menu[0, 0] = GameObject.Find("/Canvas/BtnLogin");
+        p1Menu[1, 0] = GameObject.Find("/Canvas/BtnRegister");
+
+        p1Menu[0, 0].GetComponent<Button>().Select();
+	}
+
+    void Move () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
