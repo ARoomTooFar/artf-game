f7f6ef16d5841a28cc625a6ddccf0a97289f327c
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-04-22 02:11:33 -0700

    cooldowns and health working

---
+++ Assets/1.Scripts/PlayerUI/PlayerUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour {
+
+	void Start () {
+
+		//hardcoded for the test scene
+		PlayerUIPane playerPane = new PlayerUIPane();
+		playerPane = transform.Find("Player1").gameObject.AddComponent<PlayerUIPane>();
+		playerPane.initVals("PlayerZ");
+
+		//for scenes with all 4 players
+//		setUpPlayerUIPane("Player1");
+//		setUpPlayerUIPane("Player2");
+//		setUpPlayerUIPane("Player3");
+//		setUpPlayerUIPane("Player4");
+
+	}
+
+	void setUpPlayerUIPane(string pl){
+		PlayerUIPane playerPane = new PlayerUIPane();
+		playerPane = transform.Find(pl).gameObject.AddComponent<PlayerUIPane>();
+		playerPane.initVals(pl);
+	}
+}
---
+++ Assets/1.Scripts/PlayerUI/PlayerUIPane.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PlayerUIPane : MonoBehaviour{
+	public Character c;
+	public Image hp;
+	public Image charge;
+	public Image slot1;
+	public Image slot2;
+	public Image slot3;
+
+	public void initVals(string player){
+		c = GameObject.Find(player).GetComponent("Player") as Character;
+		hp = transform.Find("HPbar/HP").gameObject.GetComponent<Image>();
+		charge = transform.Find("ChargeBar/Charge").gameObject.GetComponent<Image>();
+		slot1 = transform.Find("ActionSlots/Slot1/Ring").gameObject.GetComponent<Image>();
+		slot2 = transform.Find("ActionSlots/Slot2/Ring").gameObject.GetComponent<Image>();
+		slot3 = transform.Find("ActionSlots/Slot3/Ring").gameObject.GetComponent<Image>();
+	}
+
+	void Update(){
+		hp.fillAmount = ((float)c.stats.health / (float)c.stats.maxHealth);
+
+
+		slot1.fillAmount = Mathf.MoveTowards(slot1.fillAmount, 1f, Time.deltaTime / 5f);
+		if(slot1.fillAmount == 1f) slot1.fillAmount = 0f; //reset to 0 for demo purposes only
+
+		slot2.fillAmount = Mathf.MoveTowards(slot2.fillAmount, 1f, Time.deltaTime / 25f);
+		if(slot2.fillAmount == 1f) slot2.fillAmount = 0f; //reset to 0 for demo purposes only
+
+		slot3.fillAmount = Mathf.MoveTowards(slot3.fillAmount, 1f, Time.deltaTime / 15f);
+		if(slot3.fillAmount == 1f) slot3.fillAmount = 0f; //reset to 0 for demo purposes only
+	}
+	
+}
