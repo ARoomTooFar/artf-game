55c23678ee5121e23c16505ad7235e6d240e61f2
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-04-26 06:48:05 -0700

    Supports menu switching transition animations

--- Assets/1.Scripts/Screens/MainMenu/MainMenuCtrl.cs
+++ Assets/1.Scripts/Screens/MainMenu/MainMenuCtrl.cs
@@ -15,57 +15,54 @@ public class MainMenuCtrl : MonoBehaviour {
     private int locX = 0;
     private int locY = 0;
 
-    // menus
+    // start menu
     private GameObject[,] startMenu;
     private int startMenuWidth = 1;
     private int startMenuHeight = 2;
+	private Animator startMenuObj;
+
+	// login form
     private GameObject[,] loginForm;
     private int loginFormWidth = 1;
     private int loginFormHeight = 2;
-
-    private enum Menu
-    {
-        Start,
-        Register,
-        Login,
-        LevelSelect
-    };
+	private Animator loginFormObj;
 
 	void Start () {
         // setup start menu
         startMenu = new GameObject[startMenuHeight, startMenuWidth];
         startMenu[0, 0] = GameObject.Find("/Canvas/" + menuContainerName + "/StartMenu/BtnLogin");
         startMenu[1, 0] = GameObject.Find("/Canvas/" + menuContainerName + "/StartMenu/BtnRegister");
+		startMenuObj = GameObject.Find ("/Canvas/" + menuContainerName + "/StartMenu").GetComponent<Animator>();
 
         // setup login
         loginForm = new GameObject[startMenuHeight, startMenuWidth];
-        loginForm[0, 0] = GameObject.Find("/Canvas/" + menuContainerName + "/StartMenu/BtnLogin");
-        startMenu[1, 0] = GameObject.Find("/Canvas/" + menuContainerName + "/StartMenu/BtnRegister");
-
-        // login button press handler
+        loginForm[0, 0] = GameObject.Find("/Canvas/" + menuContainerName + "/LoginForm/BtnPrefab");
+		loginFormObj = GameObject.Find ("/Canvas/" + menuContainerName + "/LoginForm").GetComponent<Animator>();
+		
+		// login button press handler
         startMenu[0, 0].GetComponent<Button>().onClick.AddListener(() =>
         {
-            GameObject.Find("/Canvas/" + menuContainerName).GetComponent<Animator>().SetTrigger("startMenuFadeOut");
+			startMenuObj.GetComponent<Animator>().SetBool("startMenuBool", false);
+			loginFormObj.GetComponent<Animator>().SetBool("loginFormBool", true);
             //GameObject.Find("/Main Camera").GetComponent<MainMenuCamera>().slideDown = true;
-			GameObject.Find("/Canvas/" + menuContainerName).GetComponent<Animator>().SetTrigger("loginFormFadeIn");
             //menuLock = true;
         });
 
         // register button press handler
         startMenu[1, 0].GetComponent<Button>().onClick.AddListener(() =>
         {
-            Debug.Log("Register button pressed!");
-			Debug.Log(startMenu.Length);
+			startMenuObj.GetComponent<Animator>().SetBool("startMenuBool", false);
         });
 
-		// set currMenu to startMenu
+		// switch to start menu
 		currMenu = startMenu;
-
+		GameObject.Find("/Canvas/" + menuContainerName + "/StartMenu").GetComponent<Animator>().SetBool("startMenuBool", true);
         var pointer = new PointerEventData(EventSystem.current);
         ExecuteEvents.Execute(currMenu[locY, locX], pointer, ExecuteEvents.pointerEnterHandler); // highlight first button
         prevBtn = currMenu[locY, locX];
 	}
 
+	// handles menu joystick movement control
     void MenuMove (float hori, float vert) {
         if (vert == 0 && hori == 0)
         {
@@ -90,10 +87,17 @@ public class MainMenuCtrl : MonoBehaviour {
             prevBtn = currMenu[locY, locX];
         }
     }
+
+	// handles menu switching (ex: start menu transition to login form)
+	void MenuSwitch () {
+
+	}
 	
 	void Update () {
+		// check for joystick movement
         MenuMove(Input.GetAxisRaw(controls.hori), Input.GetAxisRaw(controls.vert));
 
+		// check for button press
         if (Input.GetButtonUp(controls.joyAttack) && menuLock == false)
         {
             var pointer = new PointerEventData(EventSystem.current);
