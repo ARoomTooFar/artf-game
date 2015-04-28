6fff672755abe91d87400428d91b66ec341ba44b
calvinatlan <calvinatlan@gmail.com>
2015-04-27 23:19:24 -0700

    Finished converting fodder tier 0
    All that's left is to copy the tier 0 sub-state machine and add lunge behaviour,
    and turn on group aggro for tier 4. Then hive should be a lot simpler, and then
    after that is Synth

--- Assets/1.Scripts/Units/Enemies/NewMonsters/Mobile/NewFoliantFodder.cs
+++ Assets/1.Scripts/Units/Enemies/NewMonsters/Mobile/NewFoliantFodder.cs
@@ -38,7 +38,7 @@ public class NewFoliantFodder: NewMobileEnemy {
 	}
 
 	public override void SetTierData(int tier) {
-		tier = 3;
+		tier = 0;
 		base.SetTierData (tier);
 		
 		this.stats.speed = tier < 3 ? 9 : 12;
