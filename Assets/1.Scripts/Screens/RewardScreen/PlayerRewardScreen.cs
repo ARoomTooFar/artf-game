ffa7ec291f9e8482fdc384514ddc052212e51a38
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-05-06 23:32:39 -0700

    loot list panel populating correctly

---
+++ Assets/1.Scripts/Screens/RewardScreen/PlayerRewardScreen.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PlayerRewardScreen : MonoBehaviour {
+	public Transform lootList;
+	float newRowYPos;
+	Vector2 iconDimens;
+	public Sprite icon;
+	public RectTransform lootListRect;
+
+	void Start () {
+		lootList = transform.Find("LootScroller/ScrollView/LootList");
+		lootListRect = lootList.GetComponent<RectTransform>();
+
+		iconDimens = this.GetComponent<RectTransform>().sizeDelta;
+//		iconDimens.y = this.GetComponent<RectTransform>().sizeDelta.y;
+//		iconDimens.x = this.GetComponent<RectTransform>().sizeDelta.x;
+
+		newRowYPos = 0f - iconDimens.y / 2 + 10f;
+
+		makeNewEntry(newRowYPos, "testIcon1");
+		makeNewEntry(newRowYPos,  "testIcon1");
+		makeNewEntry(newRowYPos,  "testIcon1");
+		makeNewEntry(newRowYPos,  "testIcon1");
+		makeNewEntry(newRowYPos,  "testIcon1");
+		makeNewEntry(newRowYPos,  "testIcon1");
+		makeNewEntry(newRowYPos,  "testIcon1");
+		makeNewEntry(newRowYPos,  "testIcon1");
+		makeNewEntry(newRowYPos,  "testIcon1");
+	}
+
+	void Update () {
+		print (lootListRect.rect.height);
+	}
+
+	void makeNewEntry(float y, string itemName){
+		GameObject newLootItem = Instantiate(Resources.Load("RewardScreen/LootItem")) as GameObject;
+		RectTransform lootItemRect = newLootItem.GetComponent<RectTransform>();
+
+		//grow loot list rect to accommodate more entries
+		lootListRect.sizeDelta = new Vector2(lootListRect.sizeDelta.x, lootListRect.sizeDelta.y + iconDimens.y - 40f);
+
+		//parent entry to loot list scroll pane
+		newLootItem.transform.SetParent(lootList, false);
+
+		//set its position
+		lootItemRect.anchoredPosition = new Vector2(0f + iconDimens.x / 2 - 10f, y);
+
+		//give it the proper icon
+		icon = Resources.Load<Sprite>("RewardScreen/" + itemName);
+		newLootItem.GetComponent<Image>().sprite = icon;
+
+		//increment y position for next iteration
+		newRowYPos -= iconDimens.y / 2 + 10f;
+	}
+	
+}
---
+++ Assets/1.Scripts/Screens/RewardScreen/RewardScreenSetup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class RewardScreenSetup : MonoBehaviour {
+	public Canvas rewardui;
+	
+	void Start () {
+		rewardui = GameObject.Find("RewardScreenUI").GetComponent<Canvas>();
+
+		transform.Find("p1").gameObject.AddComponent<PlayerRewardScreen>();
+//		transform.Find("p2").gameObject.AddComponent<PlayerRewardScreen>();
+//		transform.Find("p3").gameObject.AddComponent<PlayerRewardScreen>();
+//		transform.Find("p4").gameObject.AddComponent<PlayerRewardScreen>();
+
+
+	}
+}
