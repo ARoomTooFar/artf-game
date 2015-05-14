2c5d01af8f65ed6dbfeda31c970824cf5605b0ff
Scipik <fltang@ucsc.edu>
2015-05-13 22:45:33 -0700

    Foliant Fodder model and animations
    Foliant fodder animations done
    Lunge logic needs to be fixed up

--- Assets/1.Scripts/Units/Enemies/Behaviours/Base/AbilityUsage/Lunge/CheckLungeCD.cs
+++ Assets/1.Scripts/Units/Enemies/Behaviours/Base/AbilityUsage/Lunge/CheckLungeCD.cs
@@ -5,37 +5,25 @@ using UnityEngine;
 public class CheckLungeCD : LungeBehaviour {
 
 	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		if (lunge.curCoolDown <= 0) animator.SetTrigger("LungeOffCD");
-		float dis = Vector3.Distance(this.unit.transform.position, this.unit.target.transform.position);
-		if (dis >= 5) 
-		{
-			animator.SetBool ("InLungeRange", true);
-		} else {
-			animator.SetBool ("InLungeRange", false);
-		}
+		this.LungeChecker(animator);
 	}
 	
 	// This will be called once the animator has transitioned out of the state.
 	public override void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		if (lunge.curCoolDown <= 0) animator.SetTrigger("LungeOffCD");
-		float dis = Vector3.Distance(this.unit.transform.position, this.unit.target.transform.position);
-		if (dis >= 5) 
-		{
-			animator.SetBool ("InLungeRange", true);
-		} else {
-			animator.SetBool ("InLungeRange", false);
-		}
+		this.LungeChecker(animator);
 	}
 	
 	public override void OnStateUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		this.LungeChecker(animator);
+	}
+	
+	protected void LungeChecker(Animator animator) {
 		if (lunge.curCoolDown <= 0) animator.SetTrigger("LungeOffCD");
+		
+		if (this.unit.target == null) return;
+		
 		float dis = Vector3.Distance(this.unit.transform.position, this.unit.target.transform.position);
-		if (dis >= 5) 
-		{
-			Debug.Log ("should lunge");
-			animator.SetBool ("InLungeRange", true);
-		} else {
-			animator.SetBool ("InLungeRange", false);
-		}
+		if (dis >= 5) animator.SetBool ("InLungeRange", true);
+		else animator.SetBool ("InLungeRange", false);
 	}
 }
