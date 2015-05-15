8eb51a82e02cee17aa50f86b381937edf8e691a9
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-05-15 05:36:26 -0700

    gave camera zoom behavior. shrunk camera viewport to make UI be in its own frame. added vines for shadows. added more vines for background

--- Assets/1.Scripts/LevelEditor/FileIO.cs
+++ Assets/1.Scripts/LevelEditor/FileIO.cs
@@ -29,7 +29,7 @@ public class FileIO : MonoBehaviour
 
     #if UNITY_EDITOR
     private string dummyGameAcctId = "5750085036015616";
-	private string dummyLvlId = "4788030883758080";
+	private string dummyLvlId = "4909654157033472";
 	#endif
 	
 	void Start ()
