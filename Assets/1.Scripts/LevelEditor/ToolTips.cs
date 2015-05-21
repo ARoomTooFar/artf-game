85c554a23cd0db60d9c7fca01ce1f7fd38b55c05
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-05-20 23:22:18 -0700

    new tooltip system. new icons for room corner resizers.

--- Assets/1.Scripts/LevelEditor/ToolTips.cs
+++ Assets/1.Scripts/LevelEditor/ToolTips.cs
@@ -4,22 +4,59 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ToolTips : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	//tool tips for everything.
+	//Value is in the format title:description
+	Dictionary<string, string> tipTextTable = new Dictionary<string, string>(){
+		{"Button_Save", "Deploy Level:Deploys this level to your user account."},
+		{"Button_ModeToggle", "Toggle Mode:Toggles between Room Mode and Tile Mode. " +
+			"Room mode allows you to move and resize rooms. Tile Mode allows you to move and edit objects."},
+		{"Button_ZoomOut", "Zoom Out:Zoom the camera out."},
+		{"Button_ZoomIn", "Zoom In:Zoom the camera in."},
+		{"Button_Delete", "Delete:Delete selected object (hotkey: delete)."},
+		{"Button_CameraToggle", "Toggle Camera Mode:Toggle the camera between top-down mode and perspective mode."},
+	};
+
 	GameObject tt;
-	public String tipText;
+	String tipTextText;
+	String tipTitleText;
+	Text tipText;
+	Text tipTitle;
+
+	//are we using tooltips that follow mouse, or tooltips that show up in window
+	public bool usingMouseFollowToolTip = false;
 	
 	void Start() {
-		tt = Instantiate(Resources.Load("ScreenUI/ToolTip")) as GameObject;
-		tt.transform.SetParent(GameObject.Find("ScreenUI").transform);
-		Text t = tt.transform.Find("Text").GetComponent<Text>();
-		t.text = tipText;
-		tt.SetActive(false);
+
+		if(usingMouseFollowToolTip){
+			tt = Instantiate(Resources.Load("ScreenUI/ToolTip")) as GameObject;
+			tt.transform.SetParent(GameObject.Find("ScreenUI").transform);
+			Text t = tt.transform.Find("Panel/Text").GetComponent<Text>();
+			t.text = tipTextText;
+			tt.SetActive(false);
+		}else{
+			tipText = GameObject.Find("ScreenToolTipText").GetComponent<Text>();
+			tipTitle = GameObject.Find("ScreenToolTipTitle").GetComponent<Text>();
+
+			if(tipTextTable.ContainsKey(this.gameObject.name)){
+				tipTitleText =
+					tipTextTable[this.gameObject.name].Substring(0, tipTextTable[this.gameObject.name].IndexOf(":"));
+				tipTextText =
+					tipTextTable[this.gameObject.name].Substring(tipTextTable[this.gameObject.name].IndexOf(":") + 1);
+			}
+		}
 	}
+
+
 	
 	void Update() {
-		if(tt.activeSelf) {
-			makeToolTipFollowMouse();
+		if(usingMouseFollowToolTip){
+			if(tt.activeSelf) {
+				makeToolTipFollowMouse();
+			}
 		}
 	}
 
@@ -28,14 +65,25 @@ public class ToolTips : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 	}
 
 	void OnMouseEnter() {
-		if(gameObject.name == "Copy") {
-			return;
+		if(usingMouseFollowToolTip){
+			if(gameObject.name == "Copy") {
+				return;
+			}
+			tt.SetActive(true);
+		}else{
+			tipText.text = tipTextText;
+			tipTitle.text = tipTitleText;
 		}
-		tt.SetActive(true);
+
 	}
 
 	void OnMouseExit() {
-		tt.SetActive(false);
+		if(usingMouseFollowToolTip){
+			tt.SetActive(false);
+		}else{
+			tipTitle.text = "";
+			tipText.text = "";
+		}
 	}
 
 	public void OnPointerEnter(PointerEventData data) {
