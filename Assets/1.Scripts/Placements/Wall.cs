52833d9be45a596ac9393ddc41effdbc3f31c664
eazama <eazama001@gmail.com>
2015-05-20 21:35:57 -0700

    Reverted changes to Wall.cs and properly fixed null error
    Wall.cs shouldn't have been on the LevelEditor prefabs to begin with

--- Assets/1.Scripts/Placements/Wall.cs
+++ Assets/1.Scripts/Placements/Wall.cs
@@ -2,50 +2,45 @@
 using System.Collections;
 
 public class Wall : MonoBehaviour {
-
+	
 	public bool show;
 	public float disappear;
 	public float timeElapsed;
 	public GameObject stand;
-
+	public GameObject wall;
+	
 	protected virtual void Start() {
 		show = true;
 		disappear = .01f;
 		timeElapsed = 4;
 	}
-
+	
 	protected virtual void Awake() {
 	}
-
+	
 	public virtual void toggleShow() {
-		if(this.gameObject.transform.Find("Wall") != null){
-			GameObject wall = this.gameObject.transform.Find("Wall").gameObject;
-
-			foreach(Material mat in wall.GetComponent<Renderer>().materials) {
-				if(mat.HasProperty("_Color")) {
-					Color trans = mat.color;
-					trans.a = .2f;
-					mat.color = trans;
-				}
+		
+		foreach(Material mat in wall.GetComponent<Renderer>().materials) {
+			
+			if(mat.HasProperty("_Color")) {
+				Color trans = mat.color;
+				trans.a = .2f;
+				mat.color = trans;
 			}
 		}
-
+		
 		timeElapsed = 0;
 	}
-
+	
 	protected virtual void Update() {
 		if(timeElapsed < disappear) {
 			timeElapsed += Time.deltaTime;
 		} else {
-			if(this.gameObject.transform.Find("Wall") != null){
-				GameObject wall = this.gameObject.transform.Find("Wall").gameObject;
-
-				foreach(Material mat in wall.GetComponent<Renderer>().materials) {
-					if(mat.HasProperty("_Color")) {
-						Color trans = mat.color;
-						trans.a = 1f;
-						mat.color = trans;
-					}
+			foreach(Material mat in wall.GetComponent<Renderer>().materials) {
+				if(mat.HasProperty("_Color")) {
+					Color trans = mat.color;
+					trans.a = 1f;
+					mat.color = trans;
 				}
 			}
 		}
