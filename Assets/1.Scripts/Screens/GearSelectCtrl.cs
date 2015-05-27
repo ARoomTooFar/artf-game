b2b8504f5c95d7157ef378c1ae2e72b9928e4e93
Isaac Lean <isaaclean@creatibyte.com>
2015-05-27 15:55:43 -0700

    Tests image changing

--- Assets/1.Scripts/Screens/GearSelectCtrl.cs
+++ Assets/1.Scripts/Screens/GearSelectCtrl.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GearSelectCtrl : MonoBehaviour {
+	Image weaponIF;
 
 	// Use this for initialization
 	void Start () {
-	
+		Sprite newSprite = Resources.Load<Sprite>("ItemFrames/LongSword");
+		Debug.Log (newSprite);
+		weaponIF = GameObject.Find ("WeaponIF").GetComponent<Image> ();
+		Debug.Log (weaponIF);
+		Debug.Log (weaponIF.sprite);
+		weaponIF.sprite = newSprite;
+		Debug.Log (weaponIF.sprite);
 	}
 	
 	// Update is called once per frame
