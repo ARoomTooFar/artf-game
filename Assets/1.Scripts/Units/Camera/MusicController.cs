32ec8770dcb19c32cb8976f8efea9ee088e54b8f
Sunny <kiqnguye@ucsc.edu>
2015-05-29 05:16:37 -0700

    minor tweak for sound

--- Assets/1.Scripts/Units/Camera/MusicController.cs
+++ Assets/1.Scripts/Units/Camera/MusicController.cs
@@ -19,42 +19,54 @@ public class MusicController: MonoBehaviour {
 	public AudioSource environment;
 
 	void Start () {
-		GetComponent<Transform>();
 		allPlayers = FindObjectsOfType(typeof(Player)) as Player[];
-
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+		if(allPlayers.Length == 0) allPlayers = FindObjectsOfType(typeof(Player)) as Player[];
+
 		int actives = EnemyInSight ();
 
 		if (actives > 0 && !battle.isPlaying) {
-			environment.Pause ();
-			environment.volume = 0;
-			battle.Play ();
+			//environment.Pause ();
+			//environment.volume = 0;
+			if(TransitionOut(environment, 1.2f, 0)) {
+				battle.Stop ();
+				battle.Play ();
+			}
 		} else if (actives == 0 && battle.isPlaying) {
 			if (TransitionOut (battle, 0.7f, 0))
 				environment.UnPause ();
 		} else if (environment.volume < 1 && environment.isPlaying) {
-			TransitionIn(environment, 0.3f, 1);
+			TransitionIn (environment, 0.3f, 1);
+		} else if (battle.volume < 1 && battle.isPlaying) {
+			TransitionIn (battle, 0.3f, 1);
 		}
 	}
 
 	int EnemyInSight(){
 		HashSet<ARTFRoom> rooms = new HashSet<ARTFRoom> ();
 
+
 		// find all rooms that the players are in
 		foreach (Player play in allPlayers) {
+
 			rooms.Add (MapData.TheFarRooms.find(play.transform.position.Round()));
+//			Debug.Log (MapData.TheFarRooms.find(play.transform.position.Round()));
 		}
+
 		List<GameObject> MonstersIndex = MapData.MonsterBlocks.allMonsters ();
 		int numberofmonsters = 0;
 
 		// add enemy to the amount of enemies active rooms.
 		foreach (GameObject enemy in MonstersIndex) {
+			int hello = 0;
+//			Debug.Log(hello);
 			foreach(ARTFRoom room in rooms) {
-				if(room.inRoom(enemy.transform.position)) {numberofmonsters++;}
+
+				if(room.inRoom(enemy.transform.position)) {numberofmonsters++; }
 			}
 		}
 
@@ -82,8 +94,7 @@ public class MusicController: MonoBehaviour {
 		else if (musik.volume > done) musik.volume -= 0.1f * Time.deltaTime;
 		
 		if(musik.volume <= done) {
-			musik.Stop();
-			musik.volume = 1;
+			musik.Pause();
 			return true;
 		}
 		return false;
