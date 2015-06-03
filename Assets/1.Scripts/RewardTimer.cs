343bef0d7e7d8c00fa2af0ce776e4eec5cced8e3
Andrew Miller <thedr111111@yahoo.com>
2015-06-03 04:23:49 -0700

    reorganized some scripts. started on reward screen timer.

---
+++ Assets/1.Scripts/RewardTimer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RewardTimer : MonoBehaviour {
+
+	private GSManager gsm;
+	public int num; //the number that is incrimented
+
+	// Use this for initialization
+	void Start () {
+		gsm = GameObject.Find("GSManager").GetComponent<GSManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (num == 0) {
+			gsm.LoadScene ("MainMenu");
+		}
+
+	}
+
+	//adds 1 to the current number
+	void subNum() {
+		num ++;
+	}
+}
