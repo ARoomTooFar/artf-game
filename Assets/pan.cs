553c45576962eae6d42b92483ee2fa245fec8ad8
Andrew Miller <thedr111111@yahoo.com>
2015-06-09 13:12:35 -0700

    Scene for gear progression stuff

---
+++ Assets/pan.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pan : MonoBehaviour {
+
+	public GameObject target;
+	public float radius = 10f;
+	public float turnSpeed = 0.01f;
+	public float ang;
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		target.transform.position = offset;
+		ang += Time.deltaTime * turnSpeed;
+		Vector3 newPos = new Vector3 (target.transform.position.x + Mathf.Sin(ang)*radius,
+		                              transform.position.y,
+		                              target.transform.position.z + Mathf.Cos (ang)*radius);
+		transform.position = newPos;
+		transform.LookAt (target.transform);
+	}
+}
