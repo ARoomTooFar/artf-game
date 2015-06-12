e137a4a90c5cdd8c24976c5b07b9839b78f69cd6
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-06-12 06:22:49 -0700

    new player markers

---
+++ Assets/IndicatorFaceCam.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IndicatorFaceCam : MonoBehaviour {
+	public Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = GameObject.Find ("PerspectiveAngledCamera").GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<Canvas> ().transform.rotation = Quaternion.Euler (cam.transform.rotation.eulerAngles);
+	}
+}
